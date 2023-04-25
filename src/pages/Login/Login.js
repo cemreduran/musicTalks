faa2f2e6a643ab8150e1691153265d4b26c08832
Cemre <fenrizreiz@gmail.com>
2023-04-26 01:34:46 +0300

    formik added

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import Button from '../../components/Button';
 import {Formik} from 'formik';
 
 import styles from './Login.style';
 
+import Button from '../../components/Button';
 import Input from '../../components/Input';
+
+const initialFormValues = {
+  usermail: '',
+  password: '',
+};
 
 function Login({navigation}) {
   function goToSign() {
     navigation.navigate('SignPage');
+  }
+
+  function handleLoginForm(formValues) {
+    console.log(formValues);
   }
 
   return (
@@ -18,15 +27,25 @@ function Login({navigation}) {
         <Text style={styles.text_music}>music</Text>
         <Text style={styles.text_talks}>Talks</Text>
       </View>
-      <Formik>
-        <>
-          <Input placeholder="Please enter your e-mail.." />
-          <Input placeholder="Please enter your password.." />
-          <View style={styles.buttons_container}>
-            <Button text="Login" onPress={null} />
-            <Button text="Sign Up" onPress={goToSign} />
-          </View>
-        </>
+      <Formik initialValues={initialFormValues} onSubmit={handleLoginForm}>
+        {({values, handleChange, handleSubmit}) => (
+          <>
+            <Input
+              value={values.usermail}
+              onChangeText={handleChange('usermail')}
+              placeholder="Please enter your e-mail.."
+            />
+            <Input
+              value={values.password}
+              onChangeText={handleChange('password')}
+              placeholder="Please enter your password.."
+            />
+            <View style={styles.buttons_container}>
+              <Button text="Login" onPress={handleSubmit} />
+              <Button text="Sign Up" onPress={goToSign} />
+            </View>
+          </>
+        )}
       </Formik>
     </SafeAreaView>
   );
--- src/pages/Sign/Sign.js
+++ src/pages/Sign/Sign.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
-import Button from '../../components/Button';
+import {Formik} from 'formik';
 
 import styles from './Sign.style';
 
+import Button from '../../components/Button';
 import Input from '../../components/Input';
+
+const initialFormValues = {
+  usermail: '',
+  password: '',
+  repassword: '',
+};
 
 function Sign({navigation}) {
   function goToLogin() {
     navigation.goBack();
+  }
+
+  function handleLoginForm(formValues) {
+    console.log(formValues);
   }
 
   return (
@@ -17,13 +28,31 @@ function Sign({navigation}) {
         <Text style={styles.text_music}>music</Text>
         <Text style={styles.text_talks}>Talks</Text>
       </View>
-      <Input placeholder="Please enter your e-mail.." />
-      <Input placeholder="Please enter your password.." />
-      <Input placeholder="Please enter your password again.." />
-      <View style={styles.buttons_container}>
-        <Button text="Sign Up" onPress={null} />
-        <Button text="Back" onPress={goToLogin} />
-      </View>
+      <Formik initialValues={initialFormValues} onSubmit={handleLoginForm}>
+        {({values, handleChange, handleSubmit}) => (
+          <>
+            <Input
+              value={values.usermail}
+              onChangeText={handleChange('usermail')}
+              placeholder="Please enter your e-mail.."
+            />
+            <Input
+              value={values.password}
+              onChangeText={handleChange('password')}
+              placeholder="Please enter your password.."
+            />
+            <Input
+              value={values.repassword}
+              onChangeText={handleChange('repassword')}
+              placeholder="Please enter your password again.."
+            />
+            <View style={styles.buttons_container}>
+              <Button text="Sign Up" onPress={handleSubmit} />
+              <Button text="Back" onPress={goToLogin} />
+            </View>
+          </>
+        )}
+      </Formik>
     </SafeAreaView>
   );
 }
